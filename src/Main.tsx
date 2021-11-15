4924e740e2b07544c7ca977711d634d3badb8c0d
c332030 <c332030@gmail.com>
2021-11-15 17:06:32 +0800

    feat: main

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
+import {Button, Input, Layout} from "antd";
+import styles from './Main.module.scss';
 
-function Main() {
-  return (
-    <div>
-      <header>
-      </header>
-    </div>
-  );
+const {Content} = Layout;
+
+/**
+ * State 类型
+ */
+interface StateTypes {
+  url: String
+}
+
+/**
+ * <p>
+ *   Description: Main
+ * </p>
+ * @author c332030
+ * @version 1.0
+ */
+class Main extends React.Component <{}, StateTypes> {
+
+  state: StateTypes = {
+    url: ''
+  };
+
+  down = () => {
+    console.debug(`url: ${this.state.url}`);
+  }
+
+  render(): ReactNode {
+    return (
+      <Layout>
+        <Content>
+          <a className={ styles.downA } />
+          <Input placeholder={'请输入链接'}
+            onChange={ event =>
+              this.setState({
+                url: event.target.value
+              })
+            }
+          />
+          <Button onClick={ this.down }>下载</Button>
+        </Content>
+      </Layout>
+    );
+  }
 }
 
 export default Main;
